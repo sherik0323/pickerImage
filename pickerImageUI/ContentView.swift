83f413074db958932eac9b5c1636c06807251d07
Sherozbek <sherozradjabov.2004@gmail.com>
2023-12-11 17:59:55 +0500

    first commit

--- pickerImageUI/ContentView.swift
+++ pickerImageUI/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var image = UIImage(systemName: "photo")!
+    @State var isPickerShow = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack(alignment: Alignment(horizontal: .trailing, vertical: .bottom)){
+            Image(uiImage: image)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 100, height: 100)
+                .clipped()
+                .cornerRadius(20)
+            
+            Button{
+                isPickerShow.toggle()
+            }label: {
+                ZStack{
+                    Circle()
+                        .frame(width: 50, height: 50)
+                        .foregroundColor(.pink)
+                        .overlay(RoundedRectangle(cornerRadius: 25)
+                            .stroke(Color.white, lineWidth: 3))
+                    Image(systemName: "camera.fill")
+                        .foregroundColor(.white)
+                            
+                        
+                    
+                }
+            }
+            .offset(x:20, y:20)
+            .sheet(isPresented: $isPickerShow) {
+                ImagePicker(image: $image)
+            }
         }
-        .padding()
     }
 }
 
