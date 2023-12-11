4c77ff486bd8478e5f3f133254c75d4f2bb03fbc
Sherozbek <sherozradjabov.2004@gmail.com>
2023-12-11 15:50:36 +0500

    Initial Commit

---
+++ pickerImageUI/pickerImageUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  pickerImageUIApp.swift
+//  pickerImageUI
+//
+//  Created by Sherozbek on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct pickerImageUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
